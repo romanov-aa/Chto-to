b0eaf2959085bcab6647572e2d7b4d9cbdadf794
romanov-aa <LexaAkm@mail.ru>
2020-10-06 17:33:15 +1000

    Ну примеры там всякие

--- main.py
+++ main.py
@@ -1,3 +1,21 @@
 print('Это мои первые строчки кода')
 print(1+2)
 print("жОска")
+print(2+4/6*10)
+print(9//4)
+name = 10
+print(name + 4)
+j = "Максимально сильно супер-пупер мега анриал жесть как круто огогогогогого"
+m = 34
+print(name + m)
+print(f'{name}{m}')
+j = f'{m}"- это переменная"' 
+print(j)
+print(name, m)
+print(name, f'\n{m}')
+
+# name1 = int(input('Введите число '))
+# name2 = int(input('Введите число '))
+# print(name1 + name2)
+m += 1
+print(m)
